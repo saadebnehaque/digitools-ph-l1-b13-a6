b14924c051d2a72f3fbc79d93f3ec0441d881090
Saad <sebnehaque@gmail.com>
2026-09-16 00:33:38 +0600

    feat: complete project setup with Tailwind, DaisyUI, Manrope font, and responsive Navbar with cart badge

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,56 @@
+import navLogo from '../../assets/navLogo.png'
+const Navbar = () => {
+
+  const menus = ['Products', 'Features', 'Pricing', 'Testimonials', 'FAQ'];
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg aria-label="Menu" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+          </div>
+          <ul
+            tabIndex={-1}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            {
+              menus.map(menu => <li key={menu}><a className=" font-semibold">{menu}</a></li>)
+            }
+          </ul>
+        </div>
+        <div className="btn btn-ghost w-30 lg:w-40">
+          <img src={navLogo} />
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {
+            menus.map(menu => <li key={menu}><a className=" font-semibold">{menu}</a></li>)
+          }
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <div className="flex items-center gap-4 ">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle hidden lg:flex">
+            <div className="indicator">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+              <span className="badge badge-sm indicator-item bg-[#FE2E49] text-white">8</span>
+            </div>
+          </div>
+          <div className='flex'>
+
+            <a className="btn btn-ghost rounded-full ">Login</a>
+            <a className="btn rounded-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] font-semibold text-white">Get Started</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
+
+
+
+
+
